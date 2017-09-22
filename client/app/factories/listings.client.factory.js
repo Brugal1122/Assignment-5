bf714e39fb9e3e75a1bdfe3548062a1db772e99f
Your Name <anthonybrugal@yahoo.com>
2017-09-22 18:32:12 -0400

    Updates for assignment 6

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('https://git.heroku.com/nameless-peak-90953.git/api/listings');
+        return $http.get('https://git.heroku.com/nameless-peak-90953.git/api/listings/');
       },
 
       create: function(listing) {
-        return $http.post('https://git.heroku.com/nameless-peak-90953.git/api/listingss', listing);
+        return $http.post('https://git.heroku.com/nameless-peak-90953.git/api/listings/', listing);
       },
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/listings/' + id);
+        return $http.get('https://git.heroku.com/nameless-peak-90953.git/api/listings/' + id);
       },
 
       update: function(id, listing) {
-        return $http.put('http://localhost:8080/api/listings/' + id, listing);
+        return $http.put('https://git.heroku.com/nameless-peak-90953.git/api/listings/' + id, listing);
       },
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/listings/' + id);
+        return $http.delete('https://git.heroku.com/nameless-peak-90953.git/api/listings/' + id);
       }
     };
 
